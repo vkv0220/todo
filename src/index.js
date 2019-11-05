a2899c7ba801d5b748e1862b11d525306ecfd751
vkv0220 <36685410+vkv0220@users.noreply.github.com>
2019-11-05 10:56:38 +0700

    Initial comit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import joker from 'one-liner-joke';
+
+const AppHeader = () => {
+    return <h1>My Todo list</h1>;
+};
+
+const SearchPanel = () => {
+    const searchStyle = {
+        fontSize: '20px'
+    }
+    return <input style={searchStyle} placeholder='Search'></input>;
+};
+
+const ToDoList = (props) => {
+    const items = props.items;
+    return (
+        <ul>{items.map((item, index) => <li key={index}>{item}</li>)}</ul>
+    );
+};
+
+const RandomJoke = () => {
+    const joke = joker.getRandomJoke().body;
+    return (<div>{joke}</div>);
+}
+
+const App = () => {
+    return (
+        <div>
+            <AppHeader/>
+            <SearchPanel/>
+            <ToDoList items={['Make something', 'Make something else']}/>
+            <RandomJoke/>
+        </div>
+    );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
