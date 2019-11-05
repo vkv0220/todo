61d2c0c0f85e3f4e8b2302cd98d0dd9ae84fadcd
vkv0220 <36685410+vkv0220@users.noreply.github.com>
2019-11-05 22:34:53 +0700

    Delete element, unmutable state

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import AppHeader from "../app-header";
 import ToDoList from "../todo-list";
@@ -7,23 +7,38 @@ import ItemStatusFilter from "../item-status-filter";
 import RandomJoke from "../random-joke";
 import './app.css';
 
-const App = () => {
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make something', important: false, id: 2 },
-    { label: 'Make something else', important: true, id: 3 }
-  ];
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3}/>
-      <div className="top-panel d-flex">
-        <SearchPanel/>
-        <ItemStatusFilter/>
-      </div>
-      <ToDoList todos={todoData} onDeleted={ (id) => {console.log(id)}}/>
-      <RandomJoke/>
-    </div>
-  );
-};
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todoData: [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make something', important: false, id: 2 },
+      { label: 'Make something else', important: true, id: 3 }
+      ]
+    };
 
-export default App;
+    this.deleteItem = (id) => {
+      this.setState(({ todoData }) => {
+        const idx = todoData.findIndex(el => el.id === id);
+        const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx+1)];
+
+        return {todoData: newArray};
+      });
+    }
+  }
+
+  render () {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3}/>
+        <div className="top-panel d-flex">
+          <SearchPanel/>
+          <ItemStatusFilter/>
+        </div>
+        <ToDoList todos={this.state.todoData} onDeleted={this.deleteItem}/>
+        <RandomJoke/>
+      </div>
+    );
+  }
+};
--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -4,7 +4,6 @@ import TodoListItem from '../todo-list-item';
 import './todo-list.css';
 
 const TodoList = ({ todos, onDeleted }) => {
-
     const elements = todos.map((item) => {
         const { id, ...itemProps } = item;
 
